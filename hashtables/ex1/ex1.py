e41282c5137a5e73b7d09c87e038d1a0eab00d49
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-06-08 10:15:20 -0500

    completed problems 1 and 2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,9 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
-    return None
+def get_indices_of_item_weights(weights, length, limit):
+    weight_dict = {}
+    for i in range(length):
+        get_dict = weight_dict.get(limit - weights[i])
+        if get_dict is None:
+            weight_dict[weights[i]] = i
+        else:
+            return (i, get_dict)
